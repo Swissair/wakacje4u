9b9ab95390f850f6f099b173475c8bccde995c2a
Piotr <pskirski@hotmail.com>
2024-06-08 16:31:05 +0200

    playing with images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+// import assetImage from "/assets/images/hero_bg_3.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const imageUrl = new URL("/src/assets/images/hero_bg_3.jpg", import.meta.url);
 
   return (
     <>
@@ -25,11 +28,25 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <div>
+        {/* <img src="./img_boats_1.jpg" /> */}
+        <img src="/sunflower-spike/img_boats_1.jpg" />
+        <img src={imageUrl.href} alt="asset image" />
+      </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div
+        style={{
+          backgroundImage: `url('${imageUrl.href}')`,
+        }}
+      >
+        Hello world
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
