a00e9cdeeef3cea0108bf97c7385743403cd293d
Piotr <pskirski@hotmail.com>
2024-06-08 19:55:52 +0200

    starting to port items

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
 // import assetImage from "/assets/images/hero_bg_3.jpg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const imageUrl = new URL("/src/assets/images/hero_bg_3.jpg", import.meta.url);
+  //  const imageUrl = new URL("/src/assets/images/hero_bg_3.jpg", import.meta.url);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div>
-        {/* <img src="./img_boats_1.jpg" /> */}
-        <img src="/sunflower-spike/img_boats_1.jpg" />
-        <img src={imageUrl.href} alt="asset image" />
-      </div>
+      <Header />
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" Component={Home} />
+        {/* <Route path="/gallery" Component={Gallery} />
+      <Route path="/contact" Component={Contact} /> */}
+      </Routes>
 
-      <div
-        style={{
-          backgroundImage: `url('${imageUrl.href}')`,
-        }}
-      >
-        Hello world
-      </div>
+      {/* <Footer /> */}
     </>
   );
 }
